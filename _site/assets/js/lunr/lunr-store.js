d3d55246714d6ac620304e513cdce2ead10ef21c
jhheo <heojunghun@gmail.com>
2024-08-15 17:56:24 +0900

    minimal mistakes 테마변경

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome",
+        "excerpt":" ","categories": [],
+        "tags": ["TeXt"],
+        "url": "/welcome/",
+        "teaser": null
+      },{
+        "title": "test",
+        "excerpt":" ","categories": [],
+        "tags": ["TeXt"],
+        "url": "/test/",
+        "teaser": null
+      }]
